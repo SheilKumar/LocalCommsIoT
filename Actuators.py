efb957ee0a5d2950fd40eb1eeae0d6d8bcd21439
Conor Church <conorchurch42@gmail.com>
2022-04-03 14:49:12 +0100

    Trying to communicate with servo motor

--- Actuators.py
+++ Actuators.py
@@ -1,6 +1,10 @@
 # Define either work for actuation if motor recieved
 # Else simulate 
 
+import machine
+
 def do_actuation():
-    print("doing work")
+    p4 = machine.Pin(4)
+    servo = machine.PWM(p4, freq=50)
+    servo.duty(70)
     return
