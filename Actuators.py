7009801b9ffc0680a078bd2233f587606b6b2412
sheilkumar <sk17@illinois.edu>
2022-04-05 14:20:18 +0100

    Added actuation to work for certain time period

--- Actuators.py
+++ Actuators.py
@@ -1,10 +1,12 @@
 # Define either work for actuation if motor recieved
 # Else simulate 
-
+import time
 import machine
 
 def do_actuation():
     p4 = machine.Pin(4)
-    servo = machine.PWM(p4, freq=50)
-    servo.duty(70)
+    servo = machine.PWM(p4, freq=1000)
+    servo.duty(512)
+    time.sleep_ms(5000)
+    servo.freq(50)
     return
